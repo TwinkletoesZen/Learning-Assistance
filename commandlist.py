aab510023fa24ea2979afb636f28b1e16e689eed
TwinkletoesZen <contact@twinkletoeszen.com>
2022-05-30 09:16:25 -0700

    Sucessfully Implemated Multiple Pages

---
+++ commandlist.py
@@ -0,0 +1,16 @@
+
+
+
+class commands():
+
+
+
+  def calloutpress():
+    print("Pressed")
+    return
+
+  def checkpress():
+    global pressed
+    pressed = True
+    return pressed == True
+
--- main.py
+++ main.py
@@ -1,45 +1,131 @@
 
-
+from ast import arg
+from curses.panel import bottom_panel
+from distutils import command
+from commandlist import *
 from tkinter import *
-
+from functools import partial #execute code before return WTF????
 
 root = Tk()
-root.title("Learning Assistance")
-root.geometry("1000x1000")
+root.title("Monday")
+root.geometry("800x450")
+
 
 class Elder():
   def __init__(self, master):
     self.master = master
 
-
-class commands():
-
-  def calloutpress():
-    print("Button Pressed")
-
 class Learning_User(Elder):
-
   def __init__ (self, master):
     super().__init__(self)
-    global LUPage
-    LUPage = Canvas(master, width=300, height=300, bg="#7FA391").pack()
+    return 
 
-    return print("Canvas Loaded")
 
-  def Welcome(self, master):
-    Title_LUPage = Button(master, height=5, width=5, pady=10, command=commands.calloutpress).pack()
-
-    return print("BTN Loaded")
 
 
   
 
+  def Home_Page(self, master):
+    master.destroy()
+    master= Tk()
+    master.title("Monday")
+    master.geometry("800x450")
+
+
+    go_Homework_page = Button(master, text="Input \nHomework").grid(column=1, row=2)
+    go_Time_Management_page = Button(master, text="Manage \nTime", command="").grid(column=2, row=2)
+    go_Priorty_page = Button(master, text="Priorty", command="").grid(column=3, row=2)
+
+    # estimate_time = 0
+    # Hw_Completion_extimated_time = Label(
+    #   master,
+    #   text="Here is your estimated time: " + str(estimate_time),
+    # ).grid(column=1, row=1)
+
+
+
+  def Welcome_P2(self, master):
+    
+    master.destroy()
+
+    master = Tk()
+    master.title("Monday")
+    master.geometry("800x450")
+
+    grade_weighing = Label(master, text="Please Enter Each Classes Weighing")
+    grade_weighing.grid(column=1, row=1)
+    #get entry from input privously.
+
+    done = Button(master, text="Done", command= lambda: Learning_User.Home_Page(self, master))
+    done.grid(column=1, row=4)
+
+    return
+
+
+  def Welcome(self, master):
+
+    welcome_Msg = "Welcome to Twincodetoes' Learning Assistance"
+    Welcome_Label = Label(master, text=welcome_Msg)
+    Welcome_Label.grid(column=1, row=1, columnspan=3)
+
+    goal_Label = Label(master, text="What do you want to achieve using \n this Learning Assistance? \n Please Select One")
+    goal_Label.grid(column=1, row=2, columnspan=1)
+    goal_option_dict = {
+      "Get Higher Grades": 1,
+      "Stay Ahead" : 2,
+      "Learn how to Learn" :3,
+      "Time Management" :4,
+    }
+    goal_option = StringVar()
+    goal_dropD = OptionMenu(master, goal_option, *goal_option_dict)
+    goal_dropD.grid(column=3, row=2)
+    print(goal_option)
+
+    class_Label = Label(master, text="Please Enter the Class you are taking,\n separated by a comma")
+    class_Label.grid(column=1, row=3)
+    e_class = Entry(master)
+    e_class.insert(END, "Physics 11, Chemistry 12, etc.")
+    e_class.grid(column=2, row=3)
+
+
+
+    
+
+    welcome_Done = Button(master, text="Next", width=10, command= lambda: Learning_User.Welcome_P2(self, master))
+    welcome_Done.grid(column=1, row=4)
+
+
+
+
+
+
+
+
+    # Finished_Btn = Button(master, text="quit", width=10, command=partial(commands.calloutpress))
+    # Finished_Btn.grid(column=2, row=3)
+    
+
+
+
+    return print("Welcome Func Finished")
+
+
+
+  
+
+
+
+
+
+
+
+
+
 firstpage = Learning_User(root)
 
-firstpage.Welcome(LUPage)
+firstpage.Welcome(root)
 
 root.mainloop()
-
 
 
 
