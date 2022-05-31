c175a76b4c3d367defcb1b6c164b570e1cf7121c
TwinkletoesZen <contact@twinkletoeszen.com>
2022-05-31 13:53:38 -0700

    Sucess deploy Production Level Structure Organization

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,14 @@
-# from flask import Flask
+from flask import Flask
 
-# def create_app():
-#   app = Flask(__name__)
 
+
+def create_app():
+  app = Flask(__name__)
+  app.config['SECRET_KEY'] = 'ILoveTanya0313'
+
+  from .views import views
+  from .auth import auth
+
+  app.register_blueprint(views, url_prefix = "/")
+  app.register_blueprint(auth, url_prefix = "/auth")
+  return app
---
+++ website/views.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, redirect, url_for
+
+
+views = Blueprint('views', __name__)
+
+
+@views.route("/templates/index.html")
+def home():
+  return render_template("index.html")
+
+@views.route("/templates/Time.html")
+def Time():
+  return render_template("Time.html")
+
+@views.route("/templates/Homework.html")
+def Homework():
+  return render_template("Homework.html")
+
+@views.route("/templates/Priority.html")
+def Priority():
+  return render_template("Priority.html")
+
+
+@views.route("/") #Redirect must come after the Directed Code
+def root():
+  return redirect(url_for("views.home"))
