74a04a399bfcce18ce53b4d40bfa0d6d5f09f64f
TwinkletoesZen <contact@twinkletoeszen.com>
2022-06-01 12:02:54 -0700

    Session Sucessfully Implemented

--- website/views.py
+++ website/views.py
@@ -1,21 +1,52 @@
-from flask import Blueprint, render_template, redirect, url_for
-
-
+from flask import Blueprint, render_template, redirect, url_for, request, session
+# from flask_sqlalchemy import SQLAlchemy
 
 views = Blueprint('views', __name__)
-
+# views.secret_key = "test"
 
 @views.route("/templates/index.html")
 def home():
   return render_template("index.html")
 
-@views.route("/templates/Time.html ")
+@views.route("/templates/Time.html")
 def Time():
-  return render_template("Time.html")
+  timeuntilsleep = 0
+  timetocompleteallwork = ""
+  freetimeavaliable = 0
 
-@views.route("/templates/Homework.html")
+
+
+
+  return render_template("Time.html", time_to_complete_all_work=timetocompleteallwork, free_time_avaliable=freetimeavaliable, time_until_sleep=str(timeuntilsleep))
+
+
+
+
+@views.route("/templates/Homework.html", methods=["GET", "POST"])
 def Homework():
-  return render_template("Homework.html")
+  global list_of_HW
+  list_of_HW = ""
+  if request.method == "POST":
+    addedHW = request.form.get("addHW")
+    list_of_HW = addedHW.split(",")
+    session["all_HW"] = addedHW
+
+    print(session["all_HW"])
+  else:
+    print("Failed")
+
+  if "all_HW" in session:
+    addedHW = session["all_HW"]
+    list_of_HW = addedHW.split(",")
+    # if request.method == "POST":
+    #   newlyaddedhw = request.form.get("addHW")
+    #   newlyaddedlist = newlyaddedhw.split(",")
+    #   list_of_HW.append(newlyaddedlist)
+    # else:
+  #     print("Can't Append Item")
+  # else:
+    print("can't find session")
+  return render_template("Homework.html", list_of_HW = list_of_HW)
 
 @views.route("/templates/Priority.html")
 def Priority():
